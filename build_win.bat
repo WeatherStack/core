490dc00f0a5dfa2676771f8188b5d6a0a97ead61
William Dawson <William@wtdawson.info>
2026-02-14 21:37:45 +0000

    Working on build scripts

---
+++ build_win.bat
@@ -0,0 +1,34 @@
+@echo off
+
+REM Windows build script for WeatherStack Core (BATCH script)
+REM ---------------------------------------------------------
+REM Options:
+REM     build_win.bat "path/to/build.bat" Debug/Release "path/to/cmake/toolchain/file.cmake"
+REM Example:
+REM     build_win.bat build Debug
+
+if not exist %1 (
+    echo %1 does not exist, creating...
+    mkdir %1
+)
+
+if not %2 == Debug if not %2 == Release (
+    echo %2 is not a valid option
+    exit /B 1
+)
+
+cd %1
+
+if %2 == Debug (
+    echo Building with Debug configuration
+
+    cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="%3"
+    cmake --build . --config Debug
+) else if %2 == Release (
+    echo Building with Release configuration
+
+    cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="%3"
+    cmake --build . --config Release
+)
+
+cd ..
