cfb09d1794e1b39ce667f3482e3c02ff38abdb7a
William Dawson <William@wtdawson.info>
2026-03-28 12:54:20 +0000

    Set up MSYS MINGW64 for Windows compilation

---
+++ include/ws_core/routes/routes.h
@@ -0,0 +1,16 @@
+// For reference, see https://weatherstack.wtdawson.info/docs/apis/
+
+namespace WS_Core::Routes {
+    // These namespaces are defined here purely so it's eaiser to see
+    // how the Routes namespace is structured
+
+    // See https://weatherstack.wtdawson.info/apis/core_external_api/
+    //      and include/ws_core/routes/external
+    //      and src/core/routes/external
+    namespace External { };
+
+    // See https://weatherstack.wtdawson.info/apis/core_internal_api/
+    //      and include/ws_core/routes/internal
+    //      and src/core/routes/internal
+    namespace Internal { };
+};
